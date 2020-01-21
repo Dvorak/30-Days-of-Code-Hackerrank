5fdd9960848be6ee97d13c98235a4d0468e4d599
Zhiyuan ZHANG <zhiyuan_zhang92@hotmail.com>
2020-01-21 22:46:44 +0100

    Update 7_Arrays.cpp

--- 7_Arrays.cpp
+++ 7_Arrays.cpp
@@ -34,27 +34,6 @@ int main()
             cout << endl;
         }
     }
-/*
-    //solution 2
-    int main() {
-        int N;
-        cin >> N;
-
-        int arr[N];
-
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
-
-        reverse(arr, arr + N);
-
-        for (int i = 0; i < N; i++) {
-            cout << arr[i] << " ";
-        }
-
-        return 0;
-    }
-*/
     return 0;
 }
 
