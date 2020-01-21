d3483844dbf6e436c0bb8e6f4178a41e9a9a98f7
Dvorak <zhiyuan_zhang92@hotmail.com>
2020-01-22 00:13:42 +0100

    Create 8_Dictionaries_and_Maps.cpp

---
+++ 8_Dictionaries_and_Maps.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+
+    string name;
+    map<string, int> phoneBook;
+    
+    for (int i=0; i<n; i++) {
+        cin >> name;
+        cin >> phoneBook[name];
+    }
+
+    while(cin >> name) {
+        if(phoneBook[name]){
+            cout << name << "=" << phoneBook[name]<<endl;
+        }else{
+            cout << "Not found" << endl;
+        }
+
+    }
+    return 0;
+}
