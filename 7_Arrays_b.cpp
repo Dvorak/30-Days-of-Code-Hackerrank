66715ada54090b653a47f528562e2d5b01f5d398
Zhiyuan ZHANG <zhiyuan_zhang92@hotmail.com>
2020-01-21 22:47:38 +0100

    Update 7_Arrays_b.cpp

--- 7_Arrays_b.cpp
+++ 7_Arrays_b.cpp
@@ -1,1 +1,15 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    reverse(arr, arr + N);
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
